03fbbe140ce1d51901bc639fc56e5a8ccf1592be
Matt Smollinger <matt.smollinger@mapzen.com>
2016-03-09 15:47:43 -0500

    Initial project setup.

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '9.0'
+use_frameworks!
+
+#pod 'Pelias', '~> 1.0.0-beta1'
+pod 'Pelias', :path => '~/coding/git_projects/pelias-ios-sdk'
--- ios-sdk/ViewController.swift
+++ ios-sdk/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Pelias
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, APIConfigData {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    PeliasSearchManager.sharedInstance.apiKey = "search-tZu4e8k"
+    
+    let searchText = "Testing 1 2 3 4"
+    let searchConfig = PeliasSearchConfig(searchText: searchText, completionHandler: { (searchResponse) -> Void in
+      print(searchResponse)
+    })
+    PeliasSearchManager.sharedInstance.performSearch(searchConfig)
   }
 
   override func didReceiveMemoryWarning() {
