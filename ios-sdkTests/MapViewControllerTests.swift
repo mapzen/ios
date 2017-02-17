1a1ec9d059316467acb98a8f671549dfe0fff455
Sarah Lensing <sarah@mapzen.com>
2017-02-17 15:00:18 -0700

    MapViewController delegate tests

--- ios-sdkTests/MapViewControllerTests.swift
+++ ios-sdkTests/MapViewControllerTests.swift
@@ -338,4 +338,321 @@ class MapViewControllerTests: XCTestCase {
     XCTAssertNil(controller.currentAnnotations[testAnno2])
   }
 
+  func testPanEnabledByDefault() {
+    XCTAssertTrue(controller.panEnabled)
+  }
+  
+  func testPinchEnabledByDefault() {
+    XCTAssertTrue(controller.pinchEnabled)
+  }
+  
+  func testRotateEnabledByDefault() {
+    XCTAssertTrue(controller.rotateEnabled)
+  }
+  
+  func testShoveEnabledByDefault() {
+    XCTAssertTrue(controller.shoveEnabled)
+  }
+  
+  func testPanDisabledShouldNotRecognizePan() {
+    controller.panEnabled = false
+    let recognize = controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), shouldRecognizePanGesture: CGPoint())
+    XCTAssertFalse(recognize)
+  }
+  
+  func testPinchDisabledShouldNotRecognizePinch() {
+    controller.pinchEnabled = false
+    let recognize = controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), shouldRecognizePinchGesture: CGPoint())
+    XCTAssertFalse(recognize)
+  }
+  
+  func testRotateDisabledShouldNotRecognizeRotate() {
+    controller.rotateEnabled = false
+    let recognize = controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), shouldRecognizeRotationGesture: CGPoint())
+    XCTAssertFalse(recognize)
+  }
+  
+  func testShoveDisabledShouldNotRecognizeShove() {
+    controller.shoveEnabled = false
+    let recognize = controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), shouldRecognizeShoveGesture: CGPoint())
+    XCTAssertFalse(recognize)
+  }
+    
+  func testPanReceivedShouldCallDelegate() {
+    let delegate = TestPanDelegate()
+    controller.panDelegate = delegate
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizePanGesture: CGPoint())
+    XCTAssertTrue(delegate.didPanMap)
+  }
+  
+  func testPanReceivedShouldDisableCurrentLocation() {
+    controller.shouldFollowCurrentLocation = true
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizePanGesture: CGPoint())
+    XCTAssertFalse(controller.shouldFollowCurrentLocation)
+  }
+  
+  func testPanReceivedShouldDeselectFindMe() {
+    controller.findMeButton.isSelected = true
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizePanGesture: CGPoint())
+    XCTAssertFalse(controller.findMeButton.isSelected)
+  }
+  
+  func testPanDisabledShouldDisableCurrentLocation() {
+    controller.panEnabled = false
+    controller.shouldFollowCurrentLocation = true
+    let _ = controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), shouldRecognizePanGesture: CGPoint())
+    XCTAssertFalse(controller.shouldFollowCurrentLocation)
+  }
+  
+  func testPanDisabledShouldDeselectFindMe() {
+    controller.panEnabled = false
+    controller.findMeButton.isSelected = true
+    let _ = controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), shouldRecognizePanGesture: CGPoint())
+    XCTAssertFalse(controller.findMeButton.isSelected)
+  }
+  
+  func testPinchReceivedShouldCallDelegate() {
+    let delegate = TestPinchDelegate()
+    controller.pinchDelegate = delegate
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizePinchGesture: CGPoint())
+    XCTAssertTrue(delegate.didPinchMap)
+  }
+  
+  func testRotateReceivedShouldCallDelegate() {
+    let delegate = TestRotateDelegate()
+    controller.rotateDelegate = delegate
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeRotationGesture: CGPoint())
+    XCTAssertTrue(delegate.didRotateMap)
+  }
+  
+  func testShoveReceivedShouldCallDelegate() {
+    let delegate = TestShoveDelegate()
+    controller.shoveDelegate = delegate
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeShoveGesture: CGPoint())
+    XCTAssertTrue(delegate.didShoveMap)
+  }
+  
+  func testSingleTapRecognizedShouldCallDelegate() {
+    let delegate = AllEnabledGestureDelegate()
+    controller.gestureDelegate = delegate
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeSingleTapGesture: CGPoint())
+    XCTAssertTrue(delegate.singleTapReceived)
+  }
+  
+  func testDoubleTapRecognizedShouldCallDelegate() {
+    let delegate = AllEnabledGestureDelegate()
+    controller.gestureDelegate = delegate
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeDoubleTapGesture: CGPoint())
+    XCTAssertTrue(delegate.doubleTapReceived)
+  }
+  
+  func testLongPressRecognizedShouldCallDelegate() {
+    let delegate = AllEnabledGestureDelegate()
+    controller.gestureDelegate = delegate
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeLongPressGesture: CGPoint())
+    XCTAssertTrue(delegate.longPressTapReceived)
+  }
+  
+  func testSingleTapRecognizedShouldNotCallDelegate() {
+    let delegate = AllDisabledGestureDelegate()
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeSingleTapGesture: CGPoint())
+    XCTAssertFalse(delegate.singleTapReceived)
+  }
+  
+  func testDoubleTapRecognizedShouldNotCallDelegate() {
+    let delegate = AllDisabledGestureDelegate()
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeDoubleTapGesture: CGPoint())
+    XCTAssertFalse(delegate.doubleTapReceived)
+  }
+  
+  func testLongPressRecognizedShouldNotCallDelegate() {
+    let delegate = AllDisabledGestureDelegate()
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeLongPressGesture: CGPoint())
+    XCTAssertFalse(delegate.longPressTapReceived)
+  }
+  
+  func testSingleTapRecognizedPicksLabel() {
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeSingleTapGesture: CGPoint(x: 30, y: 40))
+    XCTAssertEqual(tgViewController.labelPickPosition.x, 30)
+    XCTAssertEqual(tgViewController.labelPickPosition.y, 40)
+  }
+  
+  func testSingleTapRecognizedPicksMarker() {
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeSingleTapGesture: CGPoint(x: 30, y: 40))
+    XCTAssertEqual(tgViewController.markerPickPosition.x, 30)
+    XCTAssertEqual(tgViewController.markerPickPosition.y, 40)
+  }
+  
+  func testSingleTapRecognizedPicksFeature() {
+    controller.mapView(controller.tgViewController, recognizer: UIGestureRecognizer(), didRecognizeSingleTapGesture: CGPoint(x: 30, y: 40))
+    XCTAssertEqual(tgViewController.featurePickPosition.x, 30)
+    XCTAssertEqual(tgViewController.featurePickPosition.y, 40)
+  }
+  
+  func testLoadSceneAsyncCallsLoadDelegate() {
+    let delegate = TestLoadDelegate()
+    controller.loadDelegate = delegate
+    controller.mapView(controller.tgViewController, didLoadSceneAsync: "scene")
+    XCTAssertTrue(delegate.sceneLoaded)
+  }
+  
+  func testDidCompleteLoadingCallsLoadDelegate() {
+    let delegate = TestLoadDelegate()
+    controller.loadDelegate = delegate
+    controller.mapViewDidCompleteLoading(controller.tgViewController)
+    XCTAssertTrue(delegate.loadingComplete)
+  }
+  
+  func testDidSelectLabelCallsSelectDelegate() {
+    let delegate = TestMapSelectDelegate()
+    controller.selectDelegate = delegate
+    controller.mapView(tgViewController, didSelectLabel: nil, atScreenPosition: CGPoint())
+    XCTAssertTrue(delegate.labelPicked)
+  }
+  
+  func testDidSelectFeatureCallsSelectDelegate() {
+    let delegate = TestMapSelectDelegate()
+    controller.selectDelegate = delegate
+    controller.mapView(tgViewController, didSelectFeature: nil, atScreenPosition: CGPoint())
+    XCTAssertTrue(delegate.featurePicked)
+  }
+  
+  func testDidSelectMarkerCallsSelectDelegate() {
+    let delegate = TestMapSelectDelegate()
+    controller.selectDelegate = delegate
+    controller.mapView(tgViewController, didSelectMarker: nil, atScreenPosition: CGPoint())
+    XCTAssertTrue(delegate.markerPicked)
+  }
 }
+
+class TestPanDelegate : NSObject, MapPanGestureDelegate {
+  
+  var didPanMap = false
+  
+  func mapController(_ view: MapViewController, didPanMap displacement: CGPoint) {
+    didPanMap = true
+  }
+}
+
+class TestPinchDelegate : NSObject, MapPinchGestureDelegate {
+  
+  var didPinchMap = false
+  
+  func mapController(_ view: MapViewController, didPinchMap displacement: CGPoint) {
+    didPinchMap = true
+  }
+}
+
+class TestRotateDelegate : NSObject, MapRotateGestureDelegate {
+  
+  var didRotateMap = false
+  
+  func mapController(_ view: MapViewController, didRotateMap displacement: CGPoint) {
+    didRotateMap = true
+  }
+}
+
+class TestShoveDelegate : NSObject, MapShoveGestureDelegate {
+  
+  var didShoveMap = false
+  
+  func mapController(_ view: MapViewController, didShoveMap displacement: CGPoint) {
+    didShoveMap = true
+  }
+}
+
+class AllEnabledGestureDelegate : NSObject, MapGestureDelegate {
+  
+  var singleTapReceived = false
+  var doubleTapReceived = false
+  var longPressTapReceived = false
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, shouldRecognizeSingleTapGesture location: CGPoint) -> Bool {
+    return true
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, didRecognizeSingleTapGesture location: CGPoint) {
+    singleTapReceived = true
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, shouldRecognizeDoubleTapGesture location: CGPoint) -> Bool {
+    return true
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, didRecognizeDoubleTapGesture location: CGPoint) {
+    doubleTapReceived = true
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, shouldRecognizeLongPressGesture location: CGPoint) -> Bool {
+    return true
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, didRecognizeLongPressGesture location: CGPoint) {
+    longPressTapReceived = true
+  }
+}
+
+class AllDisabledGestureDelegate : NSObject, MapGestureDelegate {
+  
+  var singleTapReceived = false
+  var doubleTapReceived = false
+  var longPressTapReceived = false
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, shouldRecognizeSingleTapGesture location: CGPoint) -> Bool {
+    return false
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, didRecognizeSingleTapGesture location: CGPoint) {
+    singleTapReceived = true
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, shouldRecognizeDoubleTapGesture location: CGPoint) -> Bool {
+    return false
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, didRecognizeDoubleTapGesture location: CGPoint) {
+    doubleTapReceived = true
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, shouldRecognizeLongPressGesture location: CGPoint) -> Bool {
+    return false
+  }
+  
+  func mapController(_ view: MapViewController, recognizer: UIGestureRecognizer, didRecognizeLongPressGesture location: CGPoint) {
+    longPressTapReceived = true
+  }
+}
+
+class TestLoadDelegate : NSObject, MapLoadDelegate {
+  
+  var sceneLoaded = false
+  var loadingComplete = false
+  
+  func mapController(_ controller: MapViewController, didLoadSceneAsync scene: String) {
+    sceneLoaded = true
+  }
+  
+  func mapControllerDidCompleteLoading(_ mapView: MapViewController) {
+    loadingComplete = true
+  }
+}
+
+class TestMapSelectDelegate : NSObject, MapSelectDelegate {
+  
+  var labelPicked = false
+  var markerPicked = false
+  var featurePicked = false
+  
+  func mapController(_ mapView: MapViewController, didSelectLabel labelPickResult: TGLabelPickResult?, atScreenPosition position: CGPoint) {
+    labelPicked = true
+  }
+  
+  func mapController(_ mapView: MapViewController, didSelectMarker markerPickResult: TGMarkerPickResult?, atScreenPosition position: CGPoint) {
+    markerPicked = true
+  }
+  
+  func mapController(_ mapView: MapViewController, didSelectFeature feature: [AnyHashable : Any]?, atScreenPosition position: CGPoint) {
+    featurePicked = true
+  }
+}
+
