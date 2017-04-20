b3243bbe41778ad9f2667d3c5f06b610fc3c2b4f
Sarah Lensing <sarah@mapzen.com>
2017-04-20 10:10:11 -0600

    Upload dsym w ipa

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 #
 if [[ ${PERFORM_NIGHTLY} ]]
   then
-    ipa distribute:hockeyapp --file "$CIRCLE_ARTIFACTS/ios-sdk.ipa" --teams "$NIGHTLY_SAMPLE_TEAM_ID" --notify --token "$HOCKEYAPP_TOKEN" --markdown --notes "CircleCI build $CIRCLE_BUILD_NUM" --commit-sha "$CIRCLE_SHA1" --build-server-url "$CIRCLE_BUILD_URL" --repository-url "$CIRCLE_REPOSITORY_URL"
+    ipa distribute:hockeyapp --file "$CIRCLE_ARTIFACTS/ios-sdk.ipa" --dsym "$CIRCLE_ARTIFACTS/ios-sdk.app.dSYM.zip" --teams "$NIGHTLY_SAMPLE_TEAM_ID" --notify --token "$HOCKEYAPP_TOKEN" --markdown --notes "CircleCI build $CIRCLE_BUILD_NUM" --commit-sha "$CIRCLE_SHA1" --build-server-url "$CIRCLE_BUILD_URL" --repository-url "$CIRCLE_REPOSITORY_URL"
 fi
