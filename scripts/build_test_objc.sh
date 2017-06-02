44cbb655618c3d2b728353dac4db4d01a5b84385
Matt Smollinger <matt.smollinger@mapzen.com>
2017-06-02 15:26:42 -0400

    Build process refactor and update to build objective-c sample

---
+++ scripts/build_test_objc.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+set -o pipefail &&
+      xcodebuild
+        -sdk iphonesimulator
+        -workspace ios-sdk.xcworkspace
+        -scheme "SampleApp-Objc"
+        -destination 'platform=iOS Simulator,name=iPhone 6,OS=latest'
+        MAPZEN_API_KEY=$MAPZEN_API_KEY
+        HOCKEY_APP_ID=$HOCKEY_APP_ID
+        clean build test |
+      tee $CIRCLE_ARTIFACTS/xcode_raw.log |
+      xcpretty --color --report junit --output $CIRCLE_TEST_REPORTS/xcode/results.xml
